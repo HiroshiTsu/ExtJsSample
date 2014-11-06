74377d7b7e5961a22fb70cbd876d23c614f4ba78
Hiroshi Tsukayama <tsukayamahiroshi@gmail.com>
2014-11-06 17:24:49 +0900

    津嘉山です

---
+++ CustomerNotes/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('CustomerNotes.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'CustomerNotes'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ CustomerNotes/build/temp/development/CustomerNotes/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\ExtJSSmple\Code\CustomerNotes\app.js
